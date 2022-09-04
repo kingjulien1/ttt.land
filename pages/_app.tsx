02732f156afc735fa151844f25f0d363703acd43
Julian Blaschke <julianblaschke@icloud.com>
2022-09-05 01:34:58 +0200

    added footer to base layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
+import Link from "next/link"
+import Footer from "@components/footer"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <main className="p-4 min-h-screen">
+        <div className="pt-20">
+          <Component {...pageProps} />
+        </div>
+      </main>
+      <Footer />
+    </>
+  )
 }
 
 export default MyApp
