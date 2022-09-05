ffb35d960cbf381a0d208339047dacb755e5580a
Julian Blaschke <julianblaschke@icloud.com>
2022-09-05 15:39:03 +0200

    adding dark mode toggle to navigation bar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,18 +1,20 @@
+import { useTheme } from "next-themes"
 import Link from "next/link"
 
 export default function NavBar() {
   return (
-    <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-black fixed w-full z-20 top-0 left-0 border-b border-purple-200 dark:border-purple-600 dark:border-opacity-25">
+    <nav className="bg-white px-4 sm:px-4 py-2.5 dark:bg-black fixed w-full z-20 top-0 left-0 border-b border-purple-200 dark:border-purple-600 dark:border-opacity-25">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
         <Link href="/">
           <a className="flex items-center">
             <span className="self-center text-2xl font-semibold whitespace-nowrap">ttt.land</span>
           </a>
         </Link>
-        <div className="flex md:order-2">
+        <div className="flex md:order-2 space-x-2 md:space-x-4">
+          <ColorModeMenu />
           <button
             type="button"
-            className="text-white bg-violet-700 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-white bg-violet-700 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:focus:ring-blue-800"
           >
             Join
           </button>
@@ -51,5 +53,31 @@ export default function NavBar() {
         </div>
       </div>
     </nav>
+  )
+}
+
+function ColorModeMenu() {
+  const { theme, setTheme } = useTheme()
+
+  return (
+    <button
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      type="button"
+      className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-fuchsia-200 dark:focus:ring-fuchsia-700 rounded-lg text-sm p-2.5"
+    >
+      {theme === "light" ? (
+        <svg id="theme-toggle-dark-icon" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+        </svg>
+      ) : (
+        <svg id="theme-toggle-light-icon" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+            fillRule="evenodd"
+            clipRule="evenodd"
+          ></path>
+        </svg>
+      )}
+    </button>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,17 @@ import type { AppProps } from "next/app"
 import Link from "next/link"
 import Footer from "@components/footer"
 import NavBar from "@components/navbar"
+import { ThemeProvider } from "next-themes"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider enableSystem={true} defaultTheme="system" attribute="class">
       <NavBar />
       <main className="p-4 min-h-screen">
         <Component {...pageProps} />
       </main>
       <Footer />
-    </>
+    </ThemeProvider>
   )
 }
 
