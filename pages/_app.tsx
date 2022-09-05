4c651145325c7003739286e2a2828c765141becf
Julian Blaschke <julianblaschke@icloud.com>
2022-09-05 22:22:32 +0200

    removing global stylesheet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import { createTheme, NextUIProvider, useTheme } from "@nextui-org/react"
 import { ThemeProvider } from "next-themes"
 import type { AppProps } from "next/app"
-import "../styles/globals.css"
 
 const lightTheme = createTheme({ type: "light" })
 
