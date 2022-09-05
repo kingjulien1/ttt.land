11a681f28511667f453f6aeb248bfb79375d0178
Julian Blaschke <julianblaschke@icloud.com>
2022-09-05 23:59:40 +0200

    updating footer component

--- components/footer.jsx
+++ components/footer.jsx
@@ -2,11 +2,11 @@ import { Card, Container, Grid, Link, Text } from "@nextui-org/react"
 
 export default function Footer() {
   return (
-    <Card variant="bordered" css={{ background: "transparent", padding: 20, borderBottom: "none" }}>
+    <Card variant="bordered" css={{ background: "transparent", padding: 20, margin: 10, borderBottom: "none", borderRight: "none", borderLeft: "none", borderRadius: "unset" }}>
       <Container fluid display="flex" justify="center">
         <Text size="$sm">
           Created with ❤️ by{" "}
-          <Link color="default" href="https://github.com/kingjulien1" style={{ display: "inline" }}>
+          <Link color="default" href="https://github.com/kingjulien1" css={{ display: "inline" }}>
             kingjulien1
           </Link>
         </Text>
