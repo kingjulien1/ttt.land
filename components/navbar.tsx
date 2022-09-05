b2c15aee3bfe5d47c4a95ddbc5611c828f64d229
Julian Blaschke <julianblaschke@icloud.com>
2022-09-05 16:04:16 +0200

    adding nextui for dynamic components

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@nextui-org/react"
 import { useTheme } from "next-themes"
 import Link from "next/link"
 
@@ -12,24 +13,14 @@ export default function NavBar() {
         </Link>
         <div className="flex md:order-2 space-x-2 md:space-x-4">
           <ColorModeMenu />
-          <button
-            type="button"
-            className="text-white bg-violet-700 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:focus:ring-blue-800"
-          >
+          <Button auto shadow color="secondary" className="bg-purple-500">
             Join
-          </button>
+          </Button>
           <button
             type="button"
             className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           >
             <span className="sr-only">Open main menu</span>
-            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path
-                fillRule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
           </button>
         </div>
         <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1">
@@ -60,9 +51,9 @@ function ColorModeMenu() {
   const { theme, setTheme } = useTheme()
 
   return (
-    <button
+    <Button
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      type="button"
+      auto
       className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-fuchsia-200 dark:focus:ring-fuchsia-700 rounded-lg text-sm p-2.5"
     >
       {theme === "light" ? (
@@ -78,6 +69,6 @@ function ColorModeMenu() {
           ></path>
         </svg>
       )}
-    </button>
+    </Button>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,18 @@ import Link from "next/link"
 import Footer from "@components/footer"
 import NavBar from "@components/navbar"
 import { ThemeProvider } from "next-themes"
+import { NextUIProvider } from "@nextui-org/react"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider enableSystem={true} defaultTheme="system" attribute="class">
-      <NavBar />
-      <main className="p-4 min-h-screen">
-        <Component {...pageProps} />
-      </main>
-      <Footer />
+      <NextUIProvider>
+        <NavBar />
+        <main className="p-4 min-h-screen">
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </NextUIProvider>
     </ThemeProvider>
   )
 }
