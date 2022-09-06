1b3ba1846c249002ae4731d9bcee19c972e6f706
Julian Blaschke <julianblaschke@icloud.com>
2022-09-07 00:53:03 +0200

    adding google authentication and user model to top navigation bar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,16 +1,20 @@
-import { Button, Link, Navbar, Switch, Text, useTheme } from "@nextui-org/react"
+import { Avatar, Button, Dropdown, Link, Loading, Modal, Navbar, Spacer, Switch, Text, useTheme } from "@nextui-org/react"
+import { signIn, signOut, useSession } from "next-auth/react"
 import { useTheme as useNextTheme } from "next-themes"
 import NextLink from "next/link"
+import { useState } from "react"
 
 const collapseItems = ["Home", "Discover", "About"]
 
 export default function Nav() {
+  const { status } = useSession()
+
   return (
     <Navbar isBordered variant="floating">
       <Navbar.Brand>
         <Navbar.Toggle showIn="xs" aria-label="toggle navigation" />
         <NextLink href="/">
-          <Link hideIn="xs" color="text" css={{ fontWeight: "$bold", fontSize: "large", paddingLeft: "$10" }}>
+          <Link hidein="xs" color="text" css={{ fontWeight: "$bold", fontSize: "large", paddingLeft: "$10" }}>
             ttt.land
           </Link>
         </NextLink>
@@ -28,9 +32,7 @@ export default function Nav() {
       </Navbar.Content>
       <Navbar.Content>
         <ColorModeToggle />
-        <Button auto shadow size="sm">
-          Join
-        </Button>
+        {{ loading: <Loading />, unauthenticated: <AuthModal />, authenticated: <UserModal /> }[status]}
       </Navbar.Content>
       <Navbar.Collapse>
         {collapseItems.map((item) => (
@@ -47,7 +49,7 @@ export default function Nav() {
 
 function ColorModeToggle() {
   const { setTheme } = useNextTheme()
-  const { isDark, type } = useTheme()
+  const { isDark } = useTheme()
 
   return (
     <Switch
@@ -76,5 +78,78 @@ function ColorModeToggle() {
         </svg>
       }
     />
+  )
+}
+
+function AuthModal() {
+  const [isVisible, setVisible] = useState(false)
+  const open = () => setVisible(true)
+  const close = () => setVisible(false)
+
+  return (
+    <>
+      <Button auto shadow size="sm" onClick={open}>
+        Join
+      </Button>
+      <Modal closeButton blur aria-labelledby="sign in" open={isVisible} onClose={close}>
+        <Modal.Header>
+          <Text id="modal-title" size={18}>
+            Join us!
+          </Text>
+        </Modal.Header>
+        <Modal.Body>
+          <Button
+            ghost
+            onPress={() => signIn("google")}
+            icon={
+              <svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
+                <path fill="none" d="M0 0h24v24H0z" />
+                <path d="M12 11h8.533c.044.385.067.78.067 1.184 0 2.734-.98 5.036-2.678 6.6-1.485 1.371-3.518 2.175-5.942 2.175A8.976 8.976 0 0 1 3 11.98 8.976 8.976 0 0 1 11.98 3c2.42 0 4.453.89 6.008 2.339L16.526 6.8C15.368 5.681 13.803 5 12 5a7 7 0 1 0 0 14c3.526 0 6.144-2.608 6.577-6H12v-2z" />
+              </svg>
+            }
+          >
+            Sign in with Google
+          </Button>
+          <Spacer />
+        </Modal.Body>
+      </Modal>
+    </>
+  )
+}
+
+function UserModal() {
+  const { data: session } = useSession()
+
+  return (
+    <Dropdown placement="bottom-right">
+      <Dropdown.Trigger>
+        <Avatar text={session.user.name} color="primary" bordered as="button" src={session?.user?.image} referrerPolicy="no-referrer" />
+      </Dropdown.Trigger>
+      <Dropdown.Menu aria-label="Avatar Actions">
+        <Dropdown.Item key="profile" css={{ height: "$18" }}>
+          <Text b color="inherit" css={{ d: "flex" }}>
+            Signed in as
+          </Text>
+          <Text b size="$xs" color="inherit" css={{ d: "flex" }}>
+            {session.user.email || session.user.name}
+          </Text>
+        </Dropdown.Item>
+        <Dropdown.Item key="settings" withDivider>
+          <NextLink href="/settings">
+            <Link color="text">Settings</Link>
+          </NextLink>
+        </Dropdown.Item>
+        <Dropdown.Item key="dash" withDivider>
+          <NextLink href="/dash">
+            <Link color="text">Dashboard</Link>
+          </NextLink>
+        </Dropdown.Item>
+        <Dropdown.Item key="logout" color="error" withDivider>
+          <Link color="error" onClick={() => signOut()}>
+            Sign Out
+          </Link>
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
   )
 }
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -43,9 +44,9 @@ export default NextAuth({
      * Google also returns a email_verified boolean property in the OAuth profile.
      * You can use this property to restrict access to people with verified accounts at a particular domain.
      */
-    /*     GoogleProvider({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }), */
+    }),
   ],
 })
