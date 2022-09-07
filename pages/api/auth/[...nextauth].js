727bb5d32a92601ab1c3efa4e0f63c1003a5a2af
Julian Blaschke <julianblaschke@icloud.com>
2022-09-07 23:40:30 +0200

    supporting discord authentication

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth"
+import DiscordProvider from "next-auth/providers/discord"
 import GoogleProvider from "next-auth/providers/google"
 import TwitterProvider from "next-auth/providers/twitter"
 
@@ -13,6 +14,10 @@ export default NextAuth({
       clientId: process.env.TWITTER_CLIENT_ID,
       clientSecret: process.env.TWITTER_CLIENT_SECRET,
       version: "2.0", // opt-in to Twitter OAuth 2.0
+    }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
     }),
   ],
 })
