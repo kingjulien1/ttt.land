8aa05a54595a10c1ba52f70a7794d5558f99db5d
Julian Blaschke <julianblaschke@icloud.com>
2022-09-08 15:41:06 +0200

    adding legal links to footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,16 +1,36 @@
-import { Card, Container, Grid, Link, Text } from "@nextui-org/react"
+import { Card, Link as NextUiLink, Row, Text } from "@nextui-org/react"
+import NextLink from "next/link"
 
 export default function Footer() {
   return (
     <Card variant="bordered" css={{ background: "transparent", padding: 20, margin: 10, borderBottom: "none", borderRight: "none", borderLeft: "none", borderRadius: "unset" }}>
-      <Container fluid display="flex" justify="center">
-        <Text size="$sm">
+      <Row css={{ mt: 20, display: "flex", textAlign: "-webkit-center", flexDirection: "column-reverse", "@xs": { flexDirection: "row", justifyContent: "space-around" } }}>
+        <Text size="$sm" css={{ mt: 30, "@xs": { mt: 0 }, justifySelf: "start", textAlign: "start" }}>
           Created with ❤️ by{" "}
-          <Link color="default" href="https://github.com/kingjulien1" css={{ display: "inline" }}>
+          <NextUiLink color="default" href="https://github.com/kingjulien1" css={{ display: "inline" }}>
             kingjulien1
-          </Link>
+          </NextUiLink>
         </Text>
-      </Container>
+        <Link href="privacy-policy">Privacy Policy</Link>
+        <Link href="terms-of-service">Terms of Service</Link>
+        <Link href="cookie-policy">Cookie Policy</Link>
+        <NextUiLink color="text" css={{ fontSize: "$sm", color: "$gray800" }} href="https://github.com/kingjulien1/ttt.land">
+          Repository
+        </NextUiLink>
+        <NextUiLink color="text" css={{ fontSize: "$sm", color: "$gray800" }} href="https://github.com/kingjulien1/ttt.land/discussions/new?category=feedback">
+          Feedback
+        </NextUiLink>
+      </Row>
     </Card>
+  )
+}
+
+function Link({ href, children }) {
+  return (
+    <NextLink href={href}>
+      <NextUiLink color="text" css={{ fontSize: "$sm", color: "$gray800" }}>
+        {children}
+      </NextUiLink>
+    </NextLink>
   )
 }
