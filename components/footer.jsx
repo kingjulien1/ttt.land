9b4433a7ff302211bf9ce93de1a5ff56dd7609e7
Julian Blaschke <julianblaschke@icloud.com>
2022-09-08 20:21:28 +0200

    fixing margin overflow bug & center links on mobile

--- components/footer.jsx
+++ components/footer.jsx
@@ -3,8 +3,18 @@ import NextLink from "next/link"
 
 export default function Footer() {
   return (
-    <Card variant="bordered" css={{ background: "transparent", py: 20, my: 10, borderBottom: "none", borderRight: "none", borderLeft: "none", borderRadius: "unset" }}>
-      <Row css={{ mt: 20, display: "flex", textAlign: "-webkit-center", flexDirection: "column-reverse", "@xs": { flexDirection: "row", justifyContent: "space-around" } }}>
+    <Card variant="bordered" css={{ background: "transparent", width: "100%", mx: 0, px: 20, py: 20, my: 10, borderBottom: "none", borderRight: "none", borderLeft: "none", borderRadius: "unset" }}>
+      <Row
+        css={{
+          mt: 20,
+          display: "flex",
+          textAlign: "-webkit-center",
+          flexDirection: "column-reverse",
+          alignItems: "center",
+          rowGap: 10,
+          "@xs": { flexDirection: "row", justifyContent: "space-around", rowGap: 0 },
+        }}
+      >
         <Text size="$sm" css={{ mt: 30, "@xs": { mt: 0 }, justifySelf: "start", textAlign: "start" }}>
           Created with ❤️ by{" "}
           <NextUiLink color="default" href="https://github.com/kingjulien1" css={{ display: "inline" }}>
