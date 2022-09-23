3f7725a6885a75af19adb51c4c0539749aeb9329
Julian Blaschke <julianblaschke@icloud.com>
2022-09-23 17:34:24 +0200

    addding remark

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Append the default value with md extensions
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 }
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
